746aef34000f7febb3f0d6bd40cdc5231148bd71
eleveyuan <eleveyuan@gmail.com>
2022-06-22 21:20:21 +0800

    modify code for preprocess data

--- README.md
+++ README.md
@@ -2,19 +2,15 @@
 PyTorch implementation of BERT in "BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding" (https://arxiv.org/abs/1810.04805)
 
 ## Requirements
-- Python 3.6+
-- [PyTorch 4.1+](http://pytorch.org/)
-- [tqdm](https://github.com/tqdm/tqdm)
+* python3
+* pytorch 
 
 All dependencies can be installed via:
-
-```
+``` shell
 pip install -r requirements.txt
 ```
 
-## Quickstart
-
-### Prepare data
+## Preprocess
 First things first, you need to prepare your data in an appropriate format. 
 Your corpus is assumed to follow the below constraints.
 
@@ -36,13 +32,35 @@ Jack and Jill went up the hill|To fetch a pail of water.|Jack fell down and brok
 
 Also, this repo includes SST-2 data in data/SST-2 directory for sentiment classification.
 
-### Build dictionary
-```
-python main.py preprocess-all --data_dir data/example/train.txt
-```
-Running the above command produces dictionary.txt file in your current directory.
 
-### Pre-train the model
+CAUTION: Whole process will take your lots of time. 
+
+### Step by Step
+
+``` shell
+python main.py extract-wiki --wiki_raw_path data/wiki-example/enwiki-latest-pages-articles.xml.bz2
+
+python main.py detect-sentences --raw_documents_path data/wiki-example/raw_documents.txt 
+
+python main.py split-sentences --sentences_detected_path data/wiki-example/sentences_detected.txt
+
+python main.py train-tokenizer --spm_input_path data/wiki-example/spm_input.txt
+
+python main.py prepare-documents --sentences_detected_path data/wiki-example/sentences_detected.txt
+
+python main.py split-train-val --prepared_documents_path data/wiki-example/prepared_documents.txt
+
+python main.py build-dictionary --train_path data/wiki-example/train.txt
+```
+when you are trianing tokenizer on the forth step, in case memory stackoverflow or other uncontrolled thing, you'd better set parameters `--input_sentence_size=5000000` and `--shuffle_input_sentence=false`. 
+
+### All-in-One
+
+``` shell
+python main.py preprocess-all --data_dir data/wiki-example/
+```
+
+## Pre-train the model
 ```
 python main.py pretrain --train_data data/example/train.txt --val_data data/example/val.txt --checkpoint_output model.pth
 ```
@@ -51,16 +69,13 @@ This step trains BERT model with unsupervised objective. Also this step does:
 - outputs model checkpoint periodically
 - reports the best checkpoint based on validation metric
 
-### Fine-tune the model
+## Fine-tune the model
 You can fine-tune pretrained BERT model with downstream task.
 For example, you can fine-tune your model with SST-2 sentiment classification task. 
 ```
 python main.py finetune --pretrained_checkpoint model.pth --train_data data/SST-2/train.tsv --val_data data/SST-2/dev.tsv
 ```
 This command also logs the procedure, outputs checkpoint, and reports the best checkpoint.
-
-## See also
-- [Transformer-pytorch](https://github.com/dreamgonfly/Transformer-pytorch) : My own implementation of Transformer. This BERT implementation is based on this repo.
 
 ## Author
 [@dreamgonfly](https://github.com/dreamgonfly)
--- bert/preprocess/preprocess.py
+++ bert/preprocess/preprocess.py
@@ -44,7 +44,7 @@ def preprocess_all(data_dir, wiki_raw_path, raw_documents_path, sentences_detect
     build_dictionary(train_path, dictionary_path)
 
 
-def tokenize(text: str, token_min_len: int, token_max_len: int, lower: bool):  # token_min_len: int, token_max_len: int,
+def tokenize(text: str, token_min_len: int, token_max_len: int, lower: bool, **_):  # token_min_len: int, token_max_len: int,
     if lower:
         text = text.lower()
     return text.split()
@@ -53,14 +53,15 @@ def tokenize(text: str, token_min_len: int, token_max_len: int, lower: bool):  #
 def extract_articles_wiki(wiki_raw_path, raw_documents_path, **_):
     wiki_corpus = WikiCorpus(wiki_raw_path, dictionary={}, tokenizer_func=tokenize, lower=False)
 
-    with open(raw_documents_path, 'w') as raw_documents_file:
+    with open(raw_documents_path, 'w', encoding='utf8') as raw_documents_file:
         for text in tqdm(wiki_corpus.get_texts()):
             document = ' '.join(text)
             raw_documents_file.write(document + '\n')
 
 
 def detect_sentences(raw_documents_path, sentences_detected_path, **_):
-    with open(raw_documents_path) as raw_documents_file, open(sentences_detected_path, 'w') as sentences_detected_file:
+    with open(raw_documents_path, encoding='utf8') as raw_documents_file, \
+            open(sentences_detected_path, 'w', encoding='utf8') as sentences_detected_file:
         for line in tqdm(raw_documents_file):
             sentences = sent_tokenize(line.strip())
             tokenized_sentences = []
@@ -77,7 +78,8 @@ def detect_sentences(raw_documents_path, sentences_detected_path, **_):
 
 
 def split_sentences(sentences_detected_path, spm_input_path, **_):
-    with open(sentences_detected_path) as sentences_detected_file, open(spm_input_path, 'w') as spm_input_file:
+    with open(sentences_detected_path, encoding='utf8') as sentences_detected_file, \
+            open(spm_input_path, 'w', encoding='utf8') as spm_input_file:
         for line in tqdm(sentences_detected_file):
             for sentence in line.strip().split('|'):
                 words = sentence.split()
@@ -87,8 +89,10 @@ def split_sentences(sentences_detected_path, spm_input_path, **_):
 
 
 def train_tokenizer(spm_input_path, spm_model_prefix, word_piece_vocab_size, **_):
+    # watch out: set input_sentence_size for test your code
     spm.SentencePieceTrainer.Train(f'--input={spm_input_path} --model_prefix={spm_model_prefix} '
-                                   f'--vocab_size={word_piece_vocab_size} --hard_vocab_limit=false')
+                                   f'--vocab_size={word_piece_vocab_size} --hard_vocab_limit=false '
+                                   f'--input_sentence_size=100000000')
 
 
 def prepare_documents(spm_model_prefix, sentences_detected_path, prepared_documents_path, **_):
@@ -96,12 +100,12 @@ def prepare_documents(spm_model_prefix, sentences_detected_path, prepared_docume
     sp_preprocessor = spm.SentencePieceProcessor()
     sp_preprocessor.Load(spm_model)
 
-    with open(sentences_detected_path) as sentences_detected_file, \
-            open(prepared_documents_path, 'w') as prepared_documents_file:
+    with open(sentences_detected_path, encoding='utf8') as sentences_detected_file, \
+            open(prepared_documents_path, 'w', encoding='utf8') as prepared_documents_file:
         for document in tqdm(sentences_detected_file):
             prepared_sentences = []
             pieces = []
-            for sentence in document.strip().split('|'):
+            for sentence in document.strip()..split('|'):
                 sentence_pieces = sp_preprocessor.EncodeAsPieces(sentence)
 
                 if len(sentence_pieces) <= 254:
@@ -125,14 +129,14 @@ def prepare_documents(spm_model_prefix, sentences_detected_path, prepared_docume
 
 
 def split_train_val(prepared_documents_path, train_path, val_path, **_):
-    with open(prepared_documents_path) as prepared_documents_file:
+    with open(prepared_documents_path, encoding='utf8') as prepared_documents_file:
         documents = prepared_documents_file.readlines()
 
     train_data, val_data = train_test_split(documents, test_size=10000)
-    with open(train_path, 'w') as train_file:
+    with open(train_path, 'w', encoding='utf8') as train_file:
         for line in train_data:
             train_file.write(line)
-    with open(val_path, 'w') as val_file:
+    with open(val_path, 'w', encoding='utf8') as val_file:
         for line in val_data:
             val_file.write(line)
 
@@ -140,7 +144,7 @@ def split_train_val(prepared_documents_path, train_path, val_path, **_):
 def build_dictionary(train_path, dictionary_path, **_):
 
     def token_generator(data_path):
-        with open(data_path) as file:
+        with open(data_path, encoding='utf8') as file:
             for document in file:
                 for sentence in document.strip().split('|'):
                     for token in sentence.split():
@@ -161,7 +165,7 @@ def add_preprocess_parser(subparsers):
     preprocess_all_parser.add_argument('--sentences_detected_path', type=str, default='sentences_detected.txt')
     preprocess_all_parser.add_argument('--spm_input_path', type=str, default='spm_input.txt')
     preprocess_all_parser.add_argument('--spm_model_prefix', type=str, default='spm')
-    preprocess_all_parser.add_argument('--word_piece_vocab_size', type=int, default=30000)
+    preprocess_all_parser.add_argument('--word_piece_vocab_size', type=int, default=10000)  # 30000
     preprocess_all_parser.add_argument('--prepared_documents_path', type=str, default='prepared_documents.txt')
     preprocess_all_parser.add_argument('--dictionary_path', type=str, default='dictionary.txt')
     preprocess_all_parser.add_argument('--train_path', type=str, default='train.txt')
@@ -169,40 +173,41 @@ def add_preprocess_parser(subparsers):
 
     extract_wiki_parser = subparsers.add_parser('extract-wiki')
     extract_wiki_parser.set_defaults(function=extract_articles_wiki)
-    extract_wiki_parser.add_argument('wiki_raw_path', type=str)
-    extract_wiki_parser.add_argument('raw_documents_path', nargs='?', type=str, default='raw_documents.txt')
+    extract_wiki_parser.add_argument('--wiki_raw_path', type=str)
+    extract_wiki_parser.add_argument('--raw_documents_path', nargs='?', type=str,
+                                     default='data/wiki-example/raw_documents.txt')
 
     detect_sentences_parser = subparsers.add_parser('detect-sentences')
     detect_sentences_parser.set_defaults(function=detect_sentences)
-    detect_sentences_parser.add_argument('raw_documents_path', type=str)
-    detect_sentences_parser.add_argument('sentences_detected_path', nargs='?', type=str,
-                                         default='sentences_detected.txt')
+    detect_sentences_parser.add_argument('--raw_documents_path', type=str)
+    detect_sentences_parser.add_argument('--sentences_detected_path', nargs='?', type=str,
+                                         default='data/wiki-example/sentences_detected.txt')
 
     split_sentences_parser = subparsers.add_parser('split-sentences')
     split_sentences_parser.set_defaults(function=split_sentences)
-    split_sentences_parser.add_argument('sentences_detected_path', type=str)
-    split_sentences_parser.add_argument('spm_input_path', nargs='?', type=str, default='spm_input.txt')
+    split_sentences_parser.add_argument('--sentences_detected_path', type=str)
+    split_sentences_parser.add_argument('--spm_input_path', nargs='?', type=str, default='data/wiki-example/spm_input.txt')
 
     train_tokenizer_parser = subparsers.add_parser('train-tokenizer')
     train_tokenizer_parser.set_defaults(function=train_tokenizer)
-    train_tokenizer_parser.add_argument('spm_input_path', type=str)
-    train_tokenizer_parser.add_argument('spm_model_prefix', nargs='?', type=str, default='spm')
-    train_tokenizer_parser.add_argument('--word_piece_vocab_size', type=int, default=30000)
+    train_tokenizer_parser.add_argument('--spm_input_path', type=str)
+    train_tokenizer_parser.add_argument('--spm_model_prefix', nargs='?', type=str, default='data/wiki-example/spm')
+    train_tokenizer_parser.add_argument('--word_piece_vocab_size', type=int, default=10000)  # 30000
 
     prepare_documents_parser = subparsers.add_parser('prepare-documents')
     prepare_documents_parser.set_defaults(function=prepare_documents)
-    prepare_documents_parser.add_argument('sentences_detected_path', type=str)
-    prepare_documents_parser.add_argument('prepared_documents_path', nargs='?', type=str,
-                                          default='prepared_documents.txt')
-    prepare_documents_parser.add_argument('--spm_model_prefix', type=str, default='spm')
+    prepare_documents_parser.add_argument('--sentences_detected_path', type=str)
+    prepare_documents_parser.add_argument('--prepared_documents_path', nargs='?', type=str,
+                                          default='data/wiki-example/prepared_documents.txt')
+    prepare_documents_parser.add_argument('--spm_model_prefix', type=str, default='data/wiki-example/spm')
 
     split_train_test_parser = subparsers.add_parser('split-train-val')
     split_train_test_parser.set_defaults(function=split_train_val)
-    split_train_test_parser.add_argument('prepared_documents_path', type=str)
-    split_train_test_parser.add_argument('train_path', nargs='?', type=str, default='train.txt')
-    split_train_test_parser.add_argument('val_path', nargs='?', type=str, default='val.txt')
+    split_train_test_parser.add_argument('--prepared_documents_path', type=str)
+    split_train_test_parser.add_argument('--train_path', nargs='?', type=str, default='data/wiki-example/train.txt')
+    split_train_test_parser.add_argument('--val_path', nargs='?', type=str, default='data/wiki-example/val.txt')
 
     build_dictionary_parser = subparsers.add_parser('build-dictionary')
     build_dictionary_parser.set_defaults(function=build_dictionary)
-    build_dictionary_parser.add_argument('train_path', type=str, default='train.txt')
-    build_dictionary_parser.add_argument('dictionary_path', nargs='?', type=str, default='dictionary.txt')
+    build_dictionary_parser.add_argument('--train_path', type=str, default='data/wiki-example/train.txt')
+    build_dictionary_parser.add_argument('--dictionary_path', nargs='?', type=str, default='data/wiki-example/dictionary.txt')
